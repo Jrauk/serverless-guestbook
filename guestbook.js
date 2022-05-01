7f909fea489b6c86f79aa95204f864f85753c5f9
Jrauk <103428723+Jrauk@users.noreply.github.com>
2022-05-01 11:25:37 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://8a3ee669.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
